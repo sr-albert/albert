b28ae150c2ed2510c42508caa5b3e8f395b25f39
Albert Nguyen <ndttam.dev@gmail.com>
2023-08-20 13:45:23 +0700

    chore: remove error element

--- src/routes/app.route.tsx
+++ src/routes/app.route.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, useRouteError } from "react-router-dom";
 import { App } from "@/app";
 import { Contact, Home, Project, actionContactSubmit } from "@/pages";
 
@@ -6,7 +6,6 @@ const appRoute = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <div>Something went wrong !</div>,
     children: [
       {
         index: true,
