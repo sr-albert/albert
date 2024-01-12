a26e9f14174dcbfdde2e6515ee9c214fb600dc9b
Albert Nguyen <ndttam.dev@gmail.com>
2024-01-12 10:04:55 +0700

    feat: prepare to use react-hook-form instead native form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import "./Contact.scss";
-import { Form, redirect } from "react-router-dom";
 import useForm from "@/utils/hooks/useForm";
+import { Form, redirect } from "react-router-dom";
+import "./Contact.scss";
 
 export async function action({ request }: any): Promise<Response> {
   const formData = await request.formData();
@@ -13,36 +13,37 @@ export async function action({ request }: any): Promise<Response> {
 }
 
 export default function Contact() {
-  const { values, handleChange, isVaild } = useForm({
-    initialValues: {
-      name: { value: "", error: "Please enter your name" },
-      email: { value: "", error: "Please enter your email address" },
-      phone: { value: "", error: "Please enter your phone number" },
-      message: { value: "", error: "Please enter your message" },
-      kind: { value: "OTHER" },
-    },
-    validate: (values: Record<string, any>) => {
-      console.log({
-        entry: "validate from Contac page",
-        values,
-      });
-      return {};
-    },
-    onSubmit: (values: Record<string, any>) => {
-      console.log(values);
-    },
-  });
+  // const { values, handleChange, isVaild } = useForm({
+  //   initialValues: {
+  //     name: { value: "", error: "" },
+  //     email: { value: "", error: "" },
+  //     phone: { value: "", error: "" },
+  //     message: { value: "", error: "" },
+  //     kind: { value: "OTHER" },
+  //   },
+  //   validate: (values: Record<string, any>) => {
+  //     console.log({
+  //       entry: "validate from Contac page",
+  //       values,
+  //     });
+  //     return {};
+  //   },
+  //   onSubmit: (values: Record<string, any>) => {
+  //     console.log(values);
+  //   },
+  // });
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    console.log({
-      entry: "onSubmit from Contact page",
-      values,
-    });
-  };
+  // const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   console.log({
+  //     entry: "onSubmit from Contact page",
+  //     values,
+  //   });
+  // };
 
   return (
     <div id="contact-container">
-      <Form
+      Contact Form
+      {/* <Form
         key="contact-form"
         method="post"
         aria-label="Contact me"
@@ -109,7 +110,74 @@ export default function Contact() {
         <button id="btn-submit" type="submit">
           Submit
         </button>
-      </Form>
+      </Form> */}
     </div>
+  );
+}
+
+function ContactForm() {
+  return (
+    <Form
+      key="contact-form"
+      method="post"
+      aria-label="Contact me"
+      action="/contact"
+    >
+      <h1>Contact me</h1>
+
+      <label htmlFor="input-full-name">Full Name</label>
+      <input
+        id="input-full-name"
+        data-testid="input-full-name"
+        name="name"
+        type="text"
+        placeholder="..."
+        aria-label="Full Name"
+      />
+
+      <label htmlFor="input-email">Email</label>
+      <input
+        id="input-email"
+        data-testid="input-email"
+        name="email"
+        type="text"
+        placeholder="..."
+        aria-label="Email"
+      />
+
+      <label htmlFor="input-phone">Phone</label>
+      <input
+        id="input-phone"
+        data-testid="input-phone"
+        name="phone"
+        type="text"
+        placeholder="+84"
+        aria-label="Phone"
+      />
+
+      <label htmlFor="select-reason">Reason</label>
+      <select
+        id="select-reason"
+        aria-disabled={true}
+        defaultValue="OTHER"
+        name="reason"
+        placeholder="..."
+      >
+        <option value="HIRING-ME">Hiring me</option>
+        <option value="OTHER">Other</option>
+      </select>
+
+      <label htmlFor="message">Message</label>
+      <textarea
+        id="message"
+        aria-label="Message"
+        name="message"
+        placeholder="..."
+      />
+
+      <button id="btn-submit" type="submit">
+        Submit
+      </button>
+    </Form>
   );
 }
