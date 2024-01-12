3c776b7a58e167ecfae94c179adfc3741888d067
Albert Nguyen <ndttam.dev@gmail.com>
2024-01-12 10:41:37 +0700

    feat: add more base components

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,29 @@
-export default function Input({ ...other }) {
-  return <input {...other} />;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  chidlren?: React.ReactNode;
+}
+
+export default function Input({ label, name, ...other }: InputProps) {
+  return (
+    <div className="input-wrapper">
+      {label && (
+        <label
+          className={name ? `input-label-${name}` : ""}
+          htmlFor={`${name ? `input-${name}` : ""}`}
+        >
+          {label}
+        </label>
+      )}
+      <input
+        name={name}
+        {...other}
+        style={{
+          width: "100%",
+          margin: "0px 0px 1.5rem 0px",
+          border: "1px solid #ffffff",
+          borderRadius: "4px",
+        }}
+      />
+    </div>
+  );
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import Input from "./Input";
 import Textarea from "./Textarea";
+import Select from "./Select";
 
-export { Input, Textarea };
+export { Input, Textarea, Select };
