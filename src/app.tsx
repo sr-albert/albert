2f2246ac14b7377f623d53fafa0ccc2ff0a92ec2
Albert Nguyen <ndttam.dev@gmail.com>
2024-01-17 14:22:24 +0700

    feat: Boundary boilerplate

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { ActionBar } from "./views";
+import { ActionBar, BoundaryView } from "./views";
 import "./app.scss";
 
 export function App() {
   return (
-    <>
+    <BoundaryView>
       <Outlet />
       <ActionBar />
-    </>
+    </BoundaryView>
   );
 }
---
+++ src/views/BoundaryView/BoundaryView.tsx
@@ -0,0 +1,15 @@
+interface BoundaryViewProps {
+  children: React.ReactNode;
+}
+export default function BoundaryView({ children }: BoundaryViewProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        position: "relative",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
