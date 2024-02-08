6a63ee8274639f42425c8460953fe27d4569ff27
Albert Nguyen <ndttam.dev@gmail.com>
2024-02-08 23:25:49 +0700

    enh: make backlink more fancy

--- src/app.theme.ts
+++ src/app.theme.ts
@@ -1,13 +1,45 @@
 import { createTheme } from "@mui/material";
 
+// Declare new variant of MuiButtonBase
+declare module "@mui/material/Button" {
+  interface ButtonPropsVariantOverrides {
+    abtn: true; // Albert's custom Button
+  }
+}
+
 export const appTheme = createTheme({
-  palette: {},
+  palette: {
+    mode: "dark",
+  },
   typography: {},
   components: {
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "abtn", color: "primary" },
+          style: {
+            backgroundColor: "light-color",
+            borderRadius: "4px",
+            boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.1)",
+            cursor: "pointer",
+            transition: "all 0.2s ease-in-out",
+            margin: "5px",
+            minHeight: "40px",
+            color: "dark-color",
+            border: "1px solid transparent",
+
+            "&:hover": {
+              transform: "scale(1.05)",
+              color: "main-color",
+            },
+          },
+        },
+      ],
+    },
     MuiPaper: {
       styleOverrides: {
         root: {
-          borderRadius: 20,
+          borderRadius: "20px",
         },
       },
     },
--- src/components/BackLink/BackLink.tsx
+++ src/components/BackLink/BackLink.tsx
@@ -1,13 +1,29 @@
+import { ArrowBackIosNew, BackspaceOutlined } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 interface Props {
-  to: string;
-  title: string;
+  to?: string;
+  title?: string;
 }
 export default function BackLink({ to = "..", title = "Back" }: Props) {
   return (
-    <NavLink to={to} className="back-link">
-      {`< `} {title}
+    <NavLink
+      to={to}
+      relative="path"
+      className="back-link"
+      style={{ color: "inherit" }}
+    >
+      <Button
+        startIcon={<ArrowBackIosNew />}
+        variant="abtn"
+        sx={{
+          width: "fit-content",
+          margin: "1rem 0",
+        }}
+      >
+        {title}
+      </Button>
     </NavLink>
   );
 }
