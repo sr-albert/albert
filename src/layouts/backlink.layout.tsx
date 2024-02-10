76972aff354f5a7971a7d83d9c68ea55928d87fb
Albert Nguyen <ndttam.dev@gmail.com>
2024-02-10 21:40:28 +0700

    feat: add BackLinkLayout component

---
+++ src/layouts/backlink.layout.tsx
@@ -0,0 +1,14 @@
+import BackLink from "@/components/BackLink";
+import { Container } from "@mui/material";
+
+interface Props {
+  children?: React.ReactNode;
+}
+export default function BackLinkLayout({ children }: Props) {
+  return (
+    <Container disableGutters>
+      <BackLink to="/" title="Home" />
+      <Container disableGutters>{children}</Container>
+    </Container>
+  );
+}
