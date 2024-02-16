1615165078fc162a06efa22941816dae1be34441
Albert Nguyen <ndttam.dev@gmail.com>
2024-02-16 15:28:25 +0700

    feat: download CV

---
+++ src/components/OpenWork/OpenWork.tsx
@@ -0,0 +1,27 @@
+import { Typography } from "@mui/material";
+import GlowingBackground from "../GlowingBr/GlowingBr";
+
+export default function OpenWork() {
+  return (
+    <GlowingBackground
+      // onClick={handleDownload}
+      sx={{
+        cursor: "pointer",
+        width: "fit-content",
+      }}
+    >
+      <Typography
+        variant="h6"
+        fontWeight="600"
+        component="a"
+        href={"/tamnguyen-fullstack.pdf"}
+        target="_blank"
+        sx={{
+          color: "inherit",
+        }}
+      >
+        DOWNLOAD CV
+      </Typography>
+    </GlowingBackground>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,21 @@
 import { ActionBar, ActionBarSocial } from "@/views";
 import "./Home.scss";
+import OpenWork from "@/components/OpenWork";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <div id="home">
       <section id="home-intro" className="force-center">
         <span>
-          <h4>Hi there</h4>
-          <h1>I am Albert,</h1> <h1> a software Engineer</h1>
+          <Typography variant="h4">Hi there</Typography>
+          <Typography variant="h2" fontWeight={800}>
+            I am Albert,
+          </Typography>
+          <Typography variant="h2" fontWeight={800}>
+            a software Engineer
+          </Typography>
+          <OpenWork />
         </span>
       </section>
 
