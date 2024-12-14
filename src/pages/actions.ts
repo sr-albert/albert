89f7416b1d5b31de681eca3c09ae301fd6dc1cfd
Albert Nguyen <ndttam.dev@gmail.com>
2024-12-14 21:20:53 +0700

    trigger build

---
+++ src/pages/actions.ts
@@ -0,0 +1,1 @@
+export { actionSubmit as actionContactSubmit } from "./Contact";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Home from "./Home/Home";
-import Contact, { actionSubmit } from "./Contact";
+import Contact from "./Contact";
 import Project from "./Project/Project";
 
 export { Home, Contact, Project };
-
-// eslint-disable-next-line react-refresh/only-export-components
-export { actionSubmit as actionContactSubmit };
